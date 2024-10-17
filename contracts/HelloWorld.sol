41fc01bc4c73810cf4269c99b1c72b028f9db7f4
jrigottiedu <juan.rigotti@bue.edu.ar>
2024-10-17 00:46:26 -0300

    commir hello world

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,20 @@
+//INicio
+// pragma solidity ^0.8.0;
+// Este es un comentario y será ignorado por el compilador
+// el ^ en la version 0.8.0 le indica al compiler que lo compile con cualquier version a partir de 0.8.0
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+
+//Este es mi primer contrato
+// Autor Juan Pablo Rigotti
+
+
+contract HelloWorld {
+    /* Este es un comentario de varias líneas que también
+    será ignorado por el compilador*/
+    string public greet = "Hello World!";
+}
+// Fin
